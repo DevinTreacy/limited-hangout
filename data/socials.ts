c1350e394e61571841a4f1fd6935b44753070e34
DevinTreacy <63381881+DevinTreacy@users.noreply.github.com>
2025-10-28 11:28:33 -0400

    update: real social media links

--- data/socials.ts
+++ data/socials.ts
@@ -1,7 +1,7 @@
 
 export const group = [
-  { label: "Instagram", url: "https://instagram.com/limitedhangout" },
-  { label: "TikTok", url: "https://tiktok.com/@limitedhangout" },
+  { label: "Instagram", url: "https://www.instagram.com/limited.hangout.tv" },
+  { label: "TikTok", url: "https://www.tiktok.com/@limited.hangout.tv" },
   { label: "YouTube", url: "https://youtube.com/@limitedhangout" }
 ];
 
@@ -9,22 +9,22 @@ export const individuals = [
   {
     name: "Devin",
     links: [
-      { label: "Instagram", url: "https://instagram.com/devin" },
-      { label: "TikTok", url: "https://tiktok.com/@devin" }
+      { label: "Instagram", url: "https://instagram.com/devintreacy" },
+      { label: "TikTok", url: "https://tiktok.com/@devintreacy" }
     ]
   },
   {
     name: "Matt",
     links: [
-      { label: "Instagram", url: "https://instagram.com/matt" },
-      { label: "TikTok", url: "https://tiktok.com/@matt" }
+      { label: "Instagram", url: "https://instagram.com/mattc_comedy" },
+      { label: "TikTok", url: "https://tiktok.com/@matt_chrzanowski" }
     ]
   },
   {
     name: "Pat",
     links: [
-      { label: "Instagram", url: "https://instagram.com/pat" },
-      { label: "TikTok", url: "https://tiktok.com/@pat" }
+      { label: "Instagram", url: "https://instagram.com/pat_lynott" },
+      { label: "TikTok", url: "https://tiktok.com/@pat_lynott" }
     ]
   }
 ];
